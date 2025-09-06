66e87faef5ddf7ac8e20f1af92561b2e7ebdc0f2
matheusg-morillo <matheus.morillo2000@gmail.com>
2025-09-06 15:17:17 -0300

    add tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './use-transition/App';
+import App from './zustand/App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!!);
 root.render(
   <React.StrictMode>
     <App />
---
+++ src/zustand/App.tsx
@@ -0,0 +1,19 @@
+
+const Item = () => {
+  return <div>Item</div>;
+}
+
+const Cart = () => {
+  return <div>Cart</div>;
+}
+
+export default () => {
+  return (
+    <div>
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+    </div>
+  );
+}
